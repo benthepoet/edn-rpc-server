d4d323ba3df725c57f7b6b47a18a6d9e87a9b39d
Ben Hanna <benpaulhanna@gmail.com>
2019-09-16 15:26:57 -0400

    Initial commit.

---
+++ test/edn_rpc_server/core_test.clj
@@ -0,0 +1,7 @@
+(ns edn-rpc-server.core-test
+  (:require [clojure.test :refer :all]
+            [edn-rpc-server.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
