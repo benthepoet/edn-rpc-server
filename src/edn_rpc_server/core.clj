fb3234bc0e4e00ad22090af45620991690c85331
Ben Hanna <benpaulhanna@gmail.com>
2019-09-16 16:33:55 -0400

    Cleaned up structure.

--- src/edn_rpc_server/core.clj
+++ src/edn_rpc_server/core.clj
@@ -1,6 +1,5 @@
 (ns edn-rpc-server.core
   (:require [clojure.edn :as edn]
-            [ring.util.request :refer [body-string]]
             [ring.util.response :as response]))
 
 (def remote-fns (atom {}))
@@ -8,15 +7,6 @@
 (defn add-remote
   [sym func]
   (swap! remote-fns assoc (keyword sym) func))
-
-(add-remote 'add
-            (fn [& args] (apply + args)))
-
-(add-remote 'multiply
-            (fn [& args] (apply * args)))
-
-(add-remote 'concat
-            (fn [& args] (apply str args)))
 
 (defn call-remote
   [sym & args]
@@ -28,8 +18,19 @@
   (let [rdr (clojure.java.io/reader (:body request))]
     (slurp rdr)))
 
+(add-remote 'add
+            (fn [& args] (apply + args)))
+
+(add-remote 'multiply
+            (fn [& args] (apply * args)))
+
+(add-remote 'concat
+            (fn [& args] (apply str args)))
+
 (defn handler
   [request]
-  (response/response
-   (pr-str (apply call-remote
-                 (edn/read-string (parse-body request))))))
+  (let [body (parse-body request)
+        expr (edn/read-string body)]
+    (if (or (nil? body) (nil? expr))
+      (response/bad-request "Invalid request.")
+      (response/response (pr-str (apply call-remote expr))))))
