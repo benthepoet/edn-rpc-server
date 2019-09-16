ddcd4a6e5d03f79ce28e36d2273fdd07b224bd60
Ben Hanna <benpaulhanna@gmail.com>
2019-09-16 16:17:38 -0400

    Added basic implementation.

--- project.clj
+++ project.clj
@@ -3,5 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [ring/ring-core "1.7.1"]
+                 [ring/ring-devel "1.7.1"]
+                 [ring/ring-jetty-adapter "1.7.1"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler edn-rpc-server.core/handler}
   :repl-options {:init-ns edn-rpc-server.core})
--- src/edn_rpc_server/core.clj
+++ src/edn_rpc_server/core.clj
@@ -1,6 +1,35 @@
-(ns edn-rpc-server.core)
+(ns edn-rpc-server.core
+  (:require [clojure.edn :as edn]
+            [ring.util.request :refer [body-string]]
+            [ring.util.response :as response]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def remote-fns (atom {}))
+
+(defn add-remote
+  [sym func]
+  (swap! remote-fns assoc (keyword sym) func))
+
+(add-remote 'add
+            (fn [& args] (apply + args)))
+
+(add-remote 'multiply
+            (fn [& args] (apply * args)))
+
+(add-remote 'concat
+            (fn [& args] (apply str args)))
+
+(defn call-remote
+  [sym & args]
+  (let [func (get @remote-fns (keyword sym))]
+    (apply func args)))
+
+(defn parse-body
+  [request]
+  (let [rdr (clojure.java.io/reader (:body request))]
+    (slurp rdr)))
+
+(defn handler
+  [request]
+  (response/response
+   (pr-str (apply call-remote
+                 (edn/read-string (parse-body request))))))
